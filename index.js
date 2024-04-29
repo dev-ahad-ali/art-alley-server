a7999e8fbfe15244c8a0aa551ea29cc290c53a0d
dev-ahad-ali <ahadali.dev@gmail.com>
2024-04-30 01:39:21 +0600

    server deploy fixed

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            'http://localhost:5173',
+            'https://art-alley.web.app',
+            'https://art-alley.firebaseapp.com',
+        ],
+    })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rocppxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
